bae2245d3b5d731d236c12b2d1430f49a8888b2b
Xlinlin <84226733@qq.com>
2018-07-19 09:27:52 +0800

    'initdir'

---
+++ 个人总结/nginx/acquire.lua
@@ -0,0 +1,12 @@
+﻿local key = KEYS[1]
+local identifier = ARGV[1]
+local lockTimeOut = ARGV[2]
+
+-- 锁定成功
+if redis.call("SETNX", key, identifier) == 1 then
+    redis.call("EXPIRE", key, lockTimeOut)
+    return 1
+elseif redis.call("TTL", key) == -1 then
+    redis.call("EXPIRE", key, lockTimeOut)
+end
+return 0
